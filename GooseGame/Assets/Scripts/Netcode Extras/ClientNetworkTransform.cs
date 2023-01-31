365c2f86e9448a215c840f7682e35a2a525119b9
Andreas.R <anzz1232@hotmail.com>
2023-01-31 10:28:56 +0100

    Extra netcode scripts

---
+++ GooseGame/Assets/Scripts/Netcode Extras/ClientNetworkTransform.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using Unity.Netcode.Components;
+
+
+[DisallowMultipleComponent]
+public class ClientNetworkTransform : NetworkTransform
+{
+    protected override bool OnIsServerAuthoritative()
+    {
+        return false;
+    }
+}
